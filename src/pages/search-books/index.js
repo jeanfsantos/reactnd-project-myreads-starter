d52f7edab8e23faf15a2c4574bb59b178e7f6261
Jean Santos <am53_jsantos@uolinc.com>
2018-10-18 19:06:40 -0300

    Removido os componentes search bar e search list

--- src/pages/search-books/index.js
+++ src/pages/search-books/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DebounceInput } from 'react-debounce-input';
 import * as BooksAPI from '@Api/BooksAPI';
-import SearchBar from '@Components/search-bar/index';
-import SearchList from '@Components/search-list/index';
+import BookItem from '@Components/book-item/index';
 
 class SearchBooks extends React.Component {
 	static propTypes = {
@@ -22,14 +22,57 @@ class SearchBooks extends React.Component {
 		});
 	};
 
+	handleShelf = (book, shelf) => {
+		BooksAPI.update(book, shelf).then(() => {
+			const books = this.state.books.map(_book => {
+				if (book.id === _book.id) {
+					_book.shelf = shelf;
+					return _book;
+				}
+				return _book;
+			});
+			this.setState({ books });
+		});
+	};
+
 	render() {
 		return (
 			<div className="search-books">
-				<SearchBar
-					onHiddenSearchPage={this.props.handleHiddenSearchPage}
-					onChangeInputSearch={this.handleInputSearch}
-				/>
-				<SearchList books={this.state.books} />
+				<div className="search-books-bar">
+					<a
+						className="close-search"
+						onClick={this.props.handleHiddenSearchPage}
+					>
+						Close
+					</a>
+					<div className="search-books-input-wrapper">
+						{/*
+							NOTES: The search from BooksAPI is limited to a particular set of search terms.
+							You can find these search terms here:
+							https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+							However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+							you don't find a specific author or title. Every search is limited by search terms.
+						*/}
+						<DebounceInput
+							minLength={2}
+							debounceTimeout={300}
+							onChange={this.handleInputSearch}
+							placeholder="Search by title or author"
+						/>
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid">
+						{this.state.books.map(book => (
+							<BookItem
+								book={book}
+								key={book.id}
+								onChangeShelf={this.handleShelf}
+							/>
+						))}
+					</ol>
+				</div>
 			</div>
 		);
 	}
